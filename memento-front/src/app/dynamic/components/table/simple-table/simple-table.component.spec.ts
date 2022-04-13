a7e03d1520db7c8fc6c8e6bff62040a49ceb070c
PasNinii <nicrannou@gmail.com>
2022-04-13 23:17:23 +0200

    fix unit test

--- memento-front/src/app/dynamic/components/table/simple-table/simple-table.component.spec.ts
+++ memento-front/src/app/dynamic/components/table/simple-table/simple-table.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockProvider } from 'ng-mocks';
+import { of } from 'rxjs';
 import { ComponentEnum, ServiceEnum } from '../../../../shared/model/interface';
 import { MovieService } from '../../../../shared/services/movie.service';
 import { SerieService } from '../../../../shared/services/serie.service';
@@ -7,28 +8,27 @@ import { ServiceFactory } from '../../../../shared/services/service.enum';
 
 import { SimpleTableComponent } from './simple-table.component';
 
-describe('SimpleTableComponent', () => {
+describe('SimpleTableComponent', async () => {
   let component: SimpleTableComponent;
   let fixture: ComponentFixture<SimpleTableComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SimpleTableComponent],
-      providers: [
-        ServiceFactory,
-        MockProvider(MovieService),
-        MockProvider(SerieService),
-      ],
+      providers: [ServiceFactory, MockProvider(MovieService), MockProvider(SerieService)],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SimpleTableComponent);
     component = fixture.componentInstance;
+    component.isLoading$ = of(false);
     component.component = {
-      type: ComponentEnum.SIMPLE_TABLE,
+      type: 'SIMPLE_TABLE',
       service: ServiceEnum.MOVIE,
       columns: [],
+      cols: 2,
+      rows: 2,
     };
     fixture.detectChanges();
   });
